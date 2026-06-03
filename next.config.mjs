e99cfef5cf21637d53101216d55c14986f1a37d5
Franek-86 <fraleccese86@gmail.com>
2026-06-03 02:52:49 +0200

    test

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
+// module.export = {
+//   webpack: (config, { server }) => {
+//     if (isServer) {
+//       config.plugins = [...config.plugins, new PrismaPlugin()];
+//     }
+//     return config;
+//   },
+// };
+
+const nextConfig = {
+  webpack: (config, { server }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
