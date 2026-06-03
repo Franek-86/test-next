d2eded44b6440b29df5c799f07866aa33b04702c
Franek-86 <fraleccese86@gmail.com>
2026-06-03 11:28:58 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
-// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 const nextConfig: NextConfig = {
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.plugins = [...config.plugin, new PrismaPlugin()];
-  //   }
-  //   return config;
-  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugin, new PrismaPlugin()];
+    }
+    return config;
+  },
   outputFileTracingIncludes: {
     "/api/**/*": ["./node_modules/.prisma/client/**/*"],
     "/*": ["./node_modules/.prisma/client/**/*"],
