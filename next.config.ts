c5e5e3d15d13abcce2252892707efa9c29d18163
Franek-86 <fraleccese86@gmail.com>
2026-06-03 07:11:22 +0200

    test

---
+++ next.config.ts
@@ -0,0 +1,38 @@
+import type { NextConfig } from "next";
+// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+const nextConfig: NextConfig = {
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     config.plugins = [...config.plugin, new PrismaPlugin()];
+  //   }
+  //   return config;
+  // },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
+  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "wsxmcfcbiibjarmtybfz.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
